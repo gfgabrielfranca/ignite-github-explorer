1ee2487a0e18c77bd5eb33603e27a6bdc05a1054
Gabriel França <dev.gabrielfranca@gmail.com>
2021-03-11 21:46:41 -0300

    Using useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
@@ -9,6 +11,16 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(setRepositories);
+  }, [])
+
+  console.log(repositories);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
